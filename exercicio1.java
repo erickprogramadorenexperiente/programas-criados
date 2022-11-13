35d1c3236adca434e576524c25580de9998b0e11
erickprogramadorenexperiente <117121640+erickprogramadorenexperiente@users.noreply.github.com>
2022-11-13 09:20:06 -0300

    Add files via upload

--- exercicio1.java
+++ exercicio1.java
@@ -1,17 +1,23 @@
-package exercicios2310;
-
-
+package exercicios_09_11;
 
 public class exercicio1 {
 
 	public static void main(String[] args) {
-		// NÚMEROS PARES DE 0 A 500
-		int i = 0;
-		do {
-			System.out.println (+i);
-			i = i+2;
-		}while (i<=500);
-            
+		// tabuadasdos números de 1 a 10.
+		
+		int resultado;
+		
+		for (int valor = 1; valor <= 10; valor = valor+1) {
+			System.out.println ();
+			System.out.println ("=== tabuada do: " +valor+ " === ");
+			System.out.println ("");
+		
+		for (int i = 1; i<=10;i = i+1) {
+			resultado = valor*i;
+			System.out.println (valor+"X"+i+"=" +resultado);
+		}
+		}
+
 	}
 
 }
